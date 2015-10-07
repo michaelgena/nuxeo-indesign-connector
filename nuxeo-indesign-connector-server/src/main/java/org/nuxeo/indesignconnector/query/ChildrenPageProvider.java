e697dc2d0e5d87b7516452ab3a332d6610458974
Julien Carsique <jcarsique@nuxeo.com>
2015-10-07 17:57:57 +0200

    code review

--- nuxeo-indesign-connector-server/src/main/java/org/nuxeo/indesignconnector/query/ChildrenPageProvider.java
+++ nuxeo-indesign-connector-server/src/main/java/org/nuxeo/indesignconnector/query/ChildrenPageProvider.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2015 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Michael GENA
+ *
+ */
 package org.nuxeo.indesignconnector.query;
 
 import java.util.List;
@@ -10,7 +27,7 @@ import org.nuxeo.ecm.core.api.impl.DocumentModelListImpl;
 import org.nuxeo.ecm.platform.query.nxql.CoreQueryDocumentPageProvider;
 
 /**
- * Created by mgena on 15/09/2015.
+ * @since 7.10
  */
 public class ChildrenPageProvider extends CoreQueryDocumentPageProvider {
 
@@ -18,31 +35,30 @@ public class ChildrenPageProvider extends CoreQueryDocumentPageProvider {
 
     private static final long serialVersionUID = 1L;
 
-    protected List<DocumentModel> currentPageDocuments;
-    
     protected int pageNumber;
 
     @Override
     public List<DocumentModel> getCurrentPage() {
-    	
-    	  DocumentModelList docs = (DocumentModelList) super.getCurrentPage();
-          String where = "(";
-          int count = docs.size();   
-          if(count > 0){
-        	  int i = 0;
-              for(DocumentModel doc : docs) {
-            	  where += "ecm:ancestorId = '" + doc.getId() + "'";
-                  i += 1;
-                  if(i < count) {
-                	  where += " OR ";
-                  }
-              }
-              where += ")";
-        	  String finalNxql = "SELECT * FROM Document WHERE ecm:mixinType = 'Picture' AND " + where + " AND ecm:isVersion = 0 AND  ecm:currentLifeCycleState != 'deleted'";          
-        	  return getCoreSession().query(finalNxql);
-          }else{
-        	  return new DocumentModelListImpl();
-          }
-       
+
+        DocumentModelList docs = (DocumentModelList) super.getCurrentPage();
+        String where = "(";
+        int count = docs.size();
+        if (count > 0) {
+            int i = 0;
+            for (DocumentModel doc : docs) {
+                where += "ecm:ancestorId = '" + doc.getId() + "'";
+                i += 1;
+                if (i < count) {
+                    where += " OR ";
+                }
+            }
+            where += ")";
+            String finalNxql = "SELECT * FROM Document WHERE ecm:mixinType = 'Picture' AND " + where
+                    + " AND ecm:isVersion = 0 AND  ecm:currentLifeCycleState != 'deleted'";
+            return getCoreSession().query(finalNxql);
+        } else {
+            return new DocumentModelListImpl();
+        }
+
     }
 }
